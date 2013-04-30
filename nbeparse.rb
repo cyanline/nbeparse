faf6619de6ddf5e09311175aea96ab2c31126192
root <gkagan@cyanline.com>
2013-05-01 03:53:36 +1000

    Added result counts.  Fixed bug that made it impossible for the user to filter results by ip. Updated formatting for easier reading. Refactored a bunch of stuff.

--- nbeparse.rb
+++ nbeparse.rb
@@ -8,16 +8,9 @@
 
 """
 
-puts """
 
-NBEParse
-
-(c) 2013 CyanLine LLC
-http://cyanline.com
-
-"""
-
-def usage()
+# Provide usage instructions
+def usage
     $stderr.puts "Uasge: ruby nbeparse <args> <nbe output>"
     $stderr.puts
     $stderr.puts "Arguments:"
@@ -30,7 +23,7 @@ def usage()
     exit(2)
 end
 
-# Check if the user wants this result type logged
+# Check if the user wants this result type logged for the final report
 def logResult?(type)
     
     if $all == true
@@ -49,6 +42,7 @@ def logResult?(type)
 
 end
 
+# Parse the IP address that the user has provided for filtering
 def onlyIP()
 
     ip = ARGV[0]
@@ -57,12 +51,129 @@ def onlyIP()
    ipv6 = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/ 
 
     if ip =~ ipv4 || ip =~ ipv6 then
+        $onlyip = ip
         ARGV.shift
     else
         usage()
     end
 
 end
+
+# Log the number of times each result type occurs
+def logType(type)
+    if type == "Security Hole"
+        $holeCount = $holeCount + 1
+    elsif type == "Log Message"
+        $logCount = $logCount + 1
+    elsif type == "Security Note"
+        $noteCount = $noteCount + 1
+    elsif type == "Security Warning"
+        $warningCount = $warningCount + 1
+    else
+        $miscCount = $miscCount + 1
+    end
+end
+
+# Collect and organize data from the raw nbe output
+def collect()
+
+    filename = ARGV[0]
+    puts "[-] opening #{filename}"
+    puts
+    f = File.open(filename, "r") # user input
+
+
+    f.each_with_index do |line, index|
+	    # don't do any of this if the line is nil
+	    if line != nil then
+            vuln = line.split('|')
+		    # Time Stamps had a array length of 4 and we dont really 
+		    # care to see timestamps
+		    # We want only results
+		    # In some cases the results provided no information and the
+		    # second line was blank so we got rid of them
+		    if vuln.size > 4  && vuln[0] == 'results' && vuln[1] != ''  then
+
+                type = vuln[5]
+                port = vuln[3]
+                descriptions = vuln[6].split("\\n")
+                number = vuln[4]
+                ip = vuln[2]
+            
+                # Check if the user wants this result logged
+                if logResult?(type) 
+
+			        # Add the ip address to the findings hash and use
+			        # the result number as the key
+                
+                    if $onlyip.length > 1
+                        if $onlyip == ip then
+			                $findings[number].push(ip)
+                        end
+                    else
+			            $findings[number].push(ip)
+                    end
+    
+	    		    # Store descriptions once in a seperate hash
+		    	    # if the description for this result number
+			        # alread exists don't store it again
+			        if !$descriptions.has_key?(number)
+
+                        descriptions.each do | line |
+                            line.gsub!("\\r", " ")
+                        end
+
+                        # Log what type of vulnerability this is for display
+                        # later
+                        logType(type)
+    
+	    			    info = [type, port, descriptions]
+		    		    $descriptions[number].push(info)
+			        end
+                end
+		    end
+	    end
+    end 
+end
+
+# Print the final report
+def printFindings()
+
+    puts "Total Findings: #{$findings.size}"
+    puts "======================"
+    puts "Log Messages: #{$logCount}"
+    puts "Security Notes: #{$noteCount}"
+    puts "Security Warnings: #{$warningCount}"
+    puts "Security Holes: #{$holeCount}"
+    puts "Other Findings: #{$miscCount}"
+    puts
+
+    $findings.each do |key, value|
+        print "==> "
+	    puts key
+	    puts $descriptions[key]
+        if $noip == false then
+            puts "Host(s) :"
+	        value.each do |e|
+                puts e
+	        end
+        end
+        2.times do puts end
+        puts "============================================================="
+        2.times do puts end
+    end
+
+end
+
+
+puts """
+
+NBEParse
+
+(c) 2013 CyanLine LLC
+http://cyanline.com
+
+"""
 
 $logs = false
 $notes = false
@@ -97,71 +208,14 @@ end
 $findings = Hash.new {|h, k| h[k] = Array.new}
 $descriptions = Hash.new {|h, k| h[k] = Array.new}
 
+$holeCount = 0
+$logCount = 0
+$noteCount = 0
+$warningCount = 0
+$miscCount = 0
 
-filename = ARGV[0]
-puts "[-] opening #{filename}"
-puts
-f = File.open(filename, "r") # user input
+# Collect and organize all vulnerabilities
+collect()
 
-f.each_with_index do |line, index|
-	# don't do any of this if the line is nil
-	if line != nil then
-        vuln = line.split('|')
-		# Time Stamps had a array length of 4 and we dont really 
-		# care to see timestamps
-		# We want only results
-		# In some cases the results provided no information and the
-		# second line was blank so we got rid of them
-		if vuln.size > 4  && vuln[0] == 'results' && vuln[1] != ''  then
-
-            type = vuln[5]
-            port = vuln[3]
-            descriptions = vuln[6].split("\\n")
-            number = vuln[4]
-            ip = vuln[2]
-            
-            # Check if the user wants this result logged
-            if logResult?(type) 
-
-			    # Add the ip address to the findings hash and use
-			    # the result number as the key
-                
-                if $onlyip.length > 1
-                    if $onlyip == ip then
-			            $findings[number].push(ip)
-                    end
-                else
-			        $findings[number].push(ip)
-                end
-    
-	    		# Store descriptions once in a seperate hash
-		    	# if the description for this result number
-			    # alread exists don't store it again
-			    if !$descriptions.has_key?(number)
-
-                    descriptions.each do | line |
-                        line.gsub!("\\r", " ")
-                    end
-
-    
-	    			info = [type, port, descriptions]
-		    		$descriptions[number].push(info)
-			    end
-            end
-		end
-	end
-end
-
-# Print everything in a readable manner
-$findings.each do |key, value|
-    print "==> "
-	puts key
-	puts $descriptions[key]
-    if $noip == false then
-        puts "Host(s) :"
-	    value.each do |e|
-            puts e
-	    end
-    end
-    2.times do puts end
-end
+# Print out results
+printFindings()
